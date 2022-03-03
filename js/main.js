cda012338849fa82615e09e411e7081d1c22f512
ext8 <99179850+ext8@users.noreply.github.com>
2022-03-03 23:36:03 +0530

    intial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// (Dark / Light) Mode Toggle
 function theme() {
     theme_icon = document.getElementById("theme").className
     document.getElementById("theme").className = theme_icon === "fas fa-sun" ? ("fa-solid fa-moon") : ("fas fa-sun")
